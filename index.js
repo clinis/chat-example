b1de17283ba06ed4f79e35bb09ba2482561b19db
clinis <dinisareias@gmail.com>
2016-11-06 01:49:07 +0000

    user list

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var users = [];
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
@@ -9,6 +11,11 @@ app.get('/', function(req, res){
 io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
+  });
+  socket.on('envia nick', function(nick){
+    io.emit('info', "New user: " + nick); console.log(nick);
+    users.push(nick);
+    io.emit('users list', users); console.log(users);
   });
 });
 
