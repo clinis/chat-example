1c84eec7b5617aa326f0e8e8a45244cd7c28bbc0
clinis <dinisareias@gmail.com>
2016-11-06 17:56:05 +0000

    fix renaming all clients when new guest enters

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ io.on('connection', function(socket){
   clients.push(socket);
 
   socket.on('start', function(){
-    io.emit('nick', "guest"+incr);
+    socket.emit('nick', "guest"+incr);
     clients[clients.indexOf(socket)].n = "guest"+incr;
     incr++;
     io.emit('users list', getUsersList());
